6a8a5cc712df69e38a5942ddcc4746dfda5f6b2f
mo <cmoxiv@gmail.com>
2025-04-07 22:06:01 +1000

    Updated pip installable.

--- setup.py
+++ setup.py
@@ -2,19 +2,13 @@ from setuptools import setup
 
 setup(
     name='dtia',
-    version='0.1.0',
+    version='0.1.1',
     description='Decision Tree Insight Analysis Tool',
     url='https://github.com/karim/dtia',
     author='Karim Hossny',
     author_email='k.hossny@kth.se',
     license='BSD 2-clause',
     packages=['dtia'],
-    # install_requires=['scikit-learn',
-    #                   'numpy',
-    #                   'tqdm',
-    #                   'graphviz',
-    #                   'pandas'],
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
